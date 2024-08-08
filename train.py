cb9c451c0f3064fbc088eaf9e6d00adea33fb9be
Richard Hamilton <hamilton.r.w@icloud.com>
2024-08-08 06:59:07 +0100

    Update train.py

--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from chatterbot.trainers import ListTrainer
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chatbot = ChatBot('MyChatBot')
-lyrics = "enter opath to .txt file containing lyrics you defined earlier"
+lyrics = "enter path to .txt file containing lyrics you defined earlier"
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.english")
 trainer = ListTrainer(chatbot)
